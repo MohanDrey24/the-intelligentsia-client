10825f9a582376df2967550ff1b712eae1ee8b2d
Mohan Drey <mohantampon@gmail.com>
2025-12-28 16:56:46 +0800

    update versions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,13 +14,17 @@ export default function Home() {
         priority
       />
 
+      <h1 className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 transform font-extrabold [-webkit-text-stroke:3px_white] sm:text-[20px] md:text-[50px] lg:text-[75px]">
+        THE INTELLIGENTSIA
+      </h1>
+
       {/* Red overlay that slides away from right to left */}
       <motion.div
         className="absolute inset-0 bg-red-500 z-10"
         initial={{ x: 0 }}
         animate={{ x: "-100%" }}
         transition={{
-          duration: 1,
+          duration: 1.0,
           ease: "easeInOut",
           delay: 0.5,
         }}
@@ -30,11 +34,11 @@ export default function Home() {
           initial={{ x: 0, y: 0 }}
           animate={{ y: 50, opacity: 0 }}
           transition={{
-            duration: 0.5,
+            duration: 0.75,
             ease: "easeInOut",
           }}
         >
-          Project Arete
+          The Intelligentsia
         </motion.h1>
       </motion.div>
     </div>
